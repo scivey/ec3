56d0089b2965f39024ede4dfb201c052d7359e7c
Scott Ivey <scott.ivey@gmail.com>
2016-05-07 18:36:48 -0400

    initial shell work

--- ec3/main_shell.py
+++ ec3/main_shell.py
@@ -1,3 +1,14 @@
-def main_shell(conf):
-    print(conf)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from __future__ import print_function, unicode_literals
+import prompt_toolkit
+from prompt_toolkit import prompt
+from prompt_toolkit.contrib.completers import WordCompleter
+
+def main_shell(conf):
+    print('conf: ', conf)
+    words = ['fish', 'cat', 'gorilla', 'ssh']
+    completer = WordCompleter(words, ignore_case=True)
+    text = prompt('yes? ', completer=completer)
+    print('yeah : "%s"' % text)
