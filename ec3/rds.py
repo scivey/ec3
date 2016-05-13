bfc95ebaa25ebf2050ab51845d8572b927f518ee
Scott Ivey <scott.ivey@gmail.com>
2016-05-12 22:30:43 -0400

    rds

---
+++ ec3/rds.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+
+def make_db_arn(account_number, aws_region, db_instance_id):
+    return 'arn:aws:%s:%s:%s' % (
+        db_instance_id, aws_region, account_number
+    )
+
